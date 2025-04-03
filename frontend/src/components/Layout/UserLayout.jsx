e5abb8ef08e9743d8eb8a734d9e066e8f9c02df8
Raunak Malhotra <rm1273503@gmail.com>
2025-04-03 17:19:37 +0530

    setup project(react-router-dom, react-icons, pages, components) and completed the Topbar

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../Common/Header'
+
+const UserLayout = () => {
+  return (
+    <>
+        {/* header section  */}
+        <Header></Header>
+
+        {/* main content  */}
+
+        {/* Footer section  */}
+    </>
+  )
+}
+
+export default UserLayout
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "rabit-red": "#ea2e0e",
+      }
+    },
+  },
+  plugins: [],
+}
